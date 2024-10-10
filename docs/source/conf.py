226f0aa1b3a825cbaeda4164789e199a48df6501
Jinseok Oh <jinseok.oh@pm.me>
2024-10-10 14:55:42 -0700

    testing html feature

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
+    'sphinxext.opengraph',
     'sphinxarg.ext'
 ]
 
@@ -33,6 +34,12 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+        'navigation_depth': 4,
+        'style_external_links': True,
+        'github_url': 'https://github.com/Infant-Neuromotor-Control-Lab/hbcd_motion_postproc'
+        }
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
